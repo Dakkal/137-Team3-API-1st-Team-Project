7b46015403f5ccce0d46d151ef065004a4a7e595
yobi721 <acousticgt7@gmail.com>
2023-04-13 10:43:55 +0900

    feature: "이넘 헤더 , 아이템 cpp 수정"

--- DefaultWindow/DefaultWindow/Item.cpp
+++ DefaultWindow/DefaultWindow/Item.cpp
@@ -23,10 +23,38 @@ int CItem::Update()
 
 void CItem::Late_Update()
 {
+	if (100 >= m_tRect.left || WINCX - 100 <= m_tRect.right)
+	{
+		m_fSpeed *= -1.f;
+
+		if (DIR_UP == m_eDir)
+		{
+			m_eDir = DIR_DOWN;
+		}
+
+		else
+		{
+			m_eDir = DIR_UP;
+		}
+	}
+
+	if (100 >= m_tRect.top || WINCY - 100 <= m_tRect.bottom)
+	{
+		if (DIR_UP == m_eDir)
+		{
+			m_eDir = DIR_DOWN;
+		}
+
+		else
+		{
+			m_eDir = DIR_UP;
+		}
+	}
 }
 
 void CItem::Render(HDC hDC)
 {
+	Ellipse(hDC, m_tRect.left, m_tRect.top, m_tRect.right, m_tRect.bottom);
 }
 
 void CItem::Release()
