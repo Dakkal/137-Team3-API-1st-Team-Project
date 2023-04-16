90c2827ca0fd9e628e3b0a74819500f81feae968
leesongsongeun <meel582@naver.com>
2023-04-16 16:53:54 +0900

    feature:"enemy 작성"

--- DefaultWindow/DefaultWindow/Enemy.cpp
+++ DefaultWindow/DefaultWindow/Enemy.cpp
@@ -3,11 +3,9 @@
 
 
 CEnemy::CEnemy()
-	:CObj(OBJECT_TYPE::MONSTER)
+	:CObj(OBJECT_TYPE::MONSTER), time(GetTickCount())
 {
-
 }
-
 
 CEnemy::~CEnemy()
 {
@@ -16,36 +14,122 @@ CEnemy::~CEnemy()
 
 void CEnemy::Initialize()
 {
-	m_tInfo = { 400.f, 100.f, 50.f, 50.f };
-	m_fSpeed = 1.f;
+	
+	m_tInfo.fX = rand() % 550 + 120;
+	m_tInfo.fY = 10.f;
+	m_tInfo.fCX = 35.f;
+	m_tInfo.fCY = 35.f;
+
+
+	m_tInfo2.fX = 300.f;
+	m_tInfo2.fY = 10.f;
+	
+	m_tInfo2.fCX = 30.f;
+	m_tInfo2.fCY = 30.f;
+
+
+	m_tInfo3.fX = 500.f;
+	m_tInfo3.fY = 10.f;
+
+	m_tInfo3.fCX = 30.f;
+	m_tInfo3.fCY = 30.f;
+
+	m_fSpeed = rand() % 5 + 1;
+	
 }
 
 int CEnemy::Update()
 {
-	m_tInfo.fX += rand() % 3 + 1;
 	m_tInfo.fY += m_fSpeed;
 
+
+	m_tInfo2.fY += m_fSpeed;
+	m_tInfo2.fX -= m_fSpeed;
+
+	m_tInfo3.fY += m_fSpeed;
+	m_tInfo3.fX += m_fSpeed;
+	
+	if (WINCY/2 < m_tInfo2.fY) {
+		m_tInfo2.fX += (m_fSpeed)*2.f;
+	}
+
+	if (WINCY / 2 < m_tInfo3.fY) {
+		m_tInfo3.fX -= (m_fSpeed)*2.f;
+	}
+
 	__super::Update_Rect();
+	Update_Rect2();
+	Update_Rect3();
 
 	return OBJ_NOEVENT;
+	
 }
 
 void CEnemy::Late_Update()
 {
-	if (m_tRect.left <= 0 || m_tRect.right >= WINCX)
-		m_fSpeed *= -1.f;
+	if (WINCY - 100 <= m_tRect.bottom)
+	{
+		m_bDead = true;
+	}
+
+	if (WINCY - 100 <= m_tRect2.bottom)
+	{
+		m_bDead = true;
+	}
+
+	if (WINCY - 100 <= m_tRect3.bottom)
+	{
+		m_bDead = true;
+	}
+
 }
 
 void CEnemy::Render(HDC hDC)
 {
-	Ellipse(hDC, m_tRect.left, m_tRect.top, m_tRect.right, m_tRect.bottom);
+	
+	Rectangle(hDC, m_tRect.left, m_tRect.top, m_tRect.right, m_tRect.bottom);
+	Ellipse(hDC, m_tRect2.left, m_tRect2.top, m_tRect2.right, m_tRect2.bottom);
+	Ellipse(hDC, m_tRect3.left, m_tRect3.top, m_tRect3.right, m_tRect3.bottom);
+	
+	if (time + 500 < GetTickCount()) {
+		
+		time = GetTickCount();
+	}
+
+}
+
+void CEnemy::Update_Rect2()
+{
+	m_tRect2.left = LONG(m_tInfo2.fX - (m_tInfo2.fCX * 0.5f));
+	m_tRect2.top = LONG(m_tInfo2.fY - (m_tInfo2.fCY * 0.5f));
+	m_tRect2.right = LONG(m_tInfo2.fX + (m_tInfo2.fCX * 0.5f));
+	m_tRect2.bottom = LONG(m_tInfo2.fY + (m_tInfo2.fCY * 0.5f));
+}
+
+void CEnemy::Update_Rect3()
+{
+	m_tRect3.left = LONG(m_tInfo3.fX - (m_tInfo3.fCX * 0.5f));
+	m_tRect3.top = LONG(m_tInfo3.fY - (m_tInfo3.fCY * 0.5f));
+	m_tRect3.right = LONG(m_tInfo3.fX + (m_tInfo3.fCX * 0.5f));
+	m_tRect3.bottom = LONG(m_tInfo3.fY + (m_tInfo3.fCY * 0.5f));
+
+}
+
+CObj * CEnemy::Enemy()
+{
+	EnemyCpy = new CEnemy;
+	EnemyCpy->Initialize();
+	return EnemyCpy;
 }
 
 void CEnemy::Release()
 {
 }
 
+
 void CEnemy::OnCollision(CObj * _pObj)
 {
 }
+
+
 
